8639a497b14c794b2479cdc36f05beddbca1545c
selfint <40059156+selfint@users.noreply.github.com>
2023-08-28 09:15:26 +0000

    update

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,11 +6,28 @@ public class App {
     private static String getSecret() {
         return "secret";
     }
+
+    private static void method(String string) {
+        Logger logger = Logger.getLogger("logger");
+
+        // BAD: user password is written to debug log
+        logger.info("Some value is " + string);
+
+        // GOOD: user password is never written to debug log
+        logger.info("Value written to log");
+    }
+
+    private static String method2(String string) {
+        return string + "!";
+    }
     
     public static void main( String[] args ) {
         Logger logger = Logger.getLogger("logger");
 
         String password = getSecret();
+
+        String string = method2(password);
+        method(string);
 
         // BAD: user password is written to debug log
         logger.info("User password is " + password);
