16126a8d7f51f5a72163ad6de78bf16407aacffe
selfint <40059156+selfint@users.noreply.github.com>
2023-08-28 10:32:52 +0000

    add spacing param

--- view_results.sh
+++ view_results.sh
@@ -10,7 +10,7 @@ jq_cmd=".runs[0].results[$results].codeFlows[0].threadFlows[0].locations | .[] |
 
 cat $t \
 | jq "$jq_cmd" \
-| awk -v src=$src '{ \
-print "cat --number " src " | sed -n -e $((" $1 "-2)),$((" $1 "+2))p \
+| awk -v src=$src -v spc=$spacing '{ \
+print "cat --number " src " | sed -n -e $((" $1 "-" spc ")),$((" $1 "+" spc "))p \
 | grep --color=always -e \"^\" -e \"$(sed -n -e " $1 "p " src ")\" \
 && echo -------------------------------------------------------------" }'
