9818970b47064404494467f049a1d165e8b93a16
selfint <40059156+selfint@users.noreply.github.com>
2023-08-28 09:05:35 +0000

    Pending changes exported from your codespace

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -6,11 +6,19 @@ public class App {
     private static String getSecret() {
         return "secret";
     }
-    
-    public static void main( String[] args ) {
+
+    private static void log(String toLog) {
+        Logger logger = Logger.getLogger("logger2");
+
+        logger.info("Logging: " + toLog);
+    }
+
+    public static void main(String[] args) {
         Logger logger = Logger.getLogger("logger");
 
         String password = getSecret();
+
+        log(password);
 
         // BAD: user password is written to debug log
         logger.info("User password is " + password);
@@ -19,3 +27,6 @@ public class App {
         logger.info("User password changed");
     }
 }
+
+// https://github.com/github/codeql/blob/9957e2683b708f22a515323cfd650cf2625f8b84/java/ql/lib/semmle/code/java/security/SensitiveLoggingQuery.qll#L67
+// https://github.com/github/codeql/blob/main/java/ql/src/Security/CWE/CWE-532/SensitiveInfoLog.ql
--- view_results.sh
+++ view_results.sh
@@ -3,9 +3,10 @@
 t=$1
 src=$2
 spacing=$3
+result=$4
 
-jq_cmd=".runs[0].results[0].codeFlows[0].threadFlows[0].locations | .[] | .location.physicalLocation.region.startLine"
+jq_cmd=".runs[0].results[$result].codeFlows[0].threadFlows[0].locations | .[] | .location.physicalLocation.region.startLine"
 
 cat $t \
     | jq "$jq_cmd" \
-    | awk -v src=$src '{print "sed -n -e $((" $1 "-2)),$((" $1 "+2))p " src " && echo ------------------------------------------------------------" }'
+    | awk -v src=$src -v spc=$spacing '{print "sed -n -e $((" $1 "-" spc ")),$((" $1 "+" spc "))p= " src " | grep --color  -e \"^\" -e \"$(sed -n -e " $1 "p " src ")\" && echo ---" }'
